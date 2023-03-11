81fb8f47e6aa133d70ac9b9d652eabb985a3f153
Alex <alex@hagl.de>
2023-03-11 18:26:03 +0100

    Added Core Project

---
+++ ContainerHive.Core/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ContainerHive.Core {
+    public static class DependencyInjection {
+
+        public static IServiceCollection AddCoreServices(this IServiceCollection services, IConfiguration configuration) {
+
+
+
+            return services;
+        }
+    }
+}

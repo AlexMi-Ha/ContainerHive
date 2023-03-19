b04f51b56b59bd579b1567d148d0de7667079a9d
Alex <alex@hagl.de>
2023-03-20 00:52:07 +0100

    Added ProjectController and WebhookController

---
+++ ContainerHive/Controllers/WebhookController.cs
@@ -0,0 +1,61 @@
+﻿using ContainerHive.Core.Common.Exceptions;
+using ContainerHive.Core.Common.Interfaces;
+using ContainerHive.Workers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContainerHive.Controllers {
+
+    [Route("webhooks")]
+    public class WebhookController : Controller {
+
+        private readonly IProjectService _projectService;
+        private readonly BackgroundWorkerQueue _backgroundWorkerQueue;
+
+        public WebhookController(IProjectService projectService, BackgroundWorkerQueue backgroundWorkerQueue) {
+            _projectService = projectService!;
+            _backgroundWorkerQueue = backgroundWorkerQueue!;
+        }
+
+        [HttpPost]
+        [Route("{id}/deploy")]
+        public async Task<IActionResult> DeployAllTask([FromRoute] string id, [FromBody] string apiToken) {
+            var cmpRes = await _projectService.CompareApiKeyAsync(id, apiToken);
+            return cmpRes.Match<IActionResult>(
+                succ => {
+                    _backgroundWorkerQueue.QueueBackgroundItem(async token => await _projectService.DeployAllAsync(id, token));
+                    return Accepted();
+                },
+                err => {
+                    if (err is ArgumentException) {
+                        return BadRequest(err.Message);
+                    }
+                    if (err is RecordNotFoundException) {
+                        return NotFound(err.Message);
+                    }
+                    return StatusCode(500, err.Message);
+                }
+                );
+        }
+
+        [HttpPost]
+        [Route("{id}/kill")]
+        public async Task<IActionResult> KillAllTask([FromRoute] string id, [FromBody] string apiToken) {
+            var cmpRes = await _projectService.CompareApiKeyAsync(id, apiToken);
+            return cmpRes.Match<IActionResult>(
+                succ => {
+                    _backgroundWorkerQueue.QueueBackgroundItem(async token => await _projectService.KillAllContainersAsync(id, token));
+                    return Accepted();
+                },
+                err => {
+                    if (err is ArgumentException) {
+                        return BadRequest(err.Message);
+                    }
+                    if (err is RecordNotFoundException) {
+                        return NotFound(err.Message);
+                    }
+                    return StatusCode(500, err.Message);
+                }
+                );
+        }
+    }
+}

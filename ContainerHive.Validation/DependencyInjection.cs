050a208734558ebffe3bb70edc577efe473203c1
Alex <alex@hagl.de>
2023-03-19 16:59:37 +0100

    Added Validation to DI

---
+++ ContainerHive.Validation/DependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿
+using ContainerHive.Validation.Validators;
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ContainerHive.Validation {
+    public static class DependencyInjection {
+
+        public static IServiceCollection AddValidationServices(this IServiceCollection services) => 
+            services.AddValidatorsFromAssemblyContaining<DeploymentValidator>(ServiceLifetime.Singleton);
+    }
+}
--- ContainerHive/Program.cs
+++ ContainerHive/Program.cs
@@ -1,5 +1,6 @@
 using ContainerHive.Core;
 using ContainerHive.Core.Datastore;
+using ContainerHive.Validation;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +11,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddValidationServices();
 builder.Services.AddCoreServices(builder.Configuration);
 
 var app = builder.Build();
